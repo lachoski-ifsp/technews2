75846c675192207434468287ab7eb4f29b6998b4
S. Lachoski A. <lachoski.abrantis@aluno.ifsp.edu.br>
2026-08-13 15:31:08 -0300

    Adicionado o .env

--- technews2/app.js
+++ technews2/app.js
@@ -65,5 +65,5 @@ app.get('/contato', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log('Servidor rodando em: http://localhost:${PORT}');
+    console.log('Servidor rodando em: http://localhost:', PORT);
 });
